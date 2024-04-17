99fd94977d59615fb4189538b20c143c0b17d463
duxphp <admin@duxphp.com>
2024-04-18 00:40:17 +0800

    update

--- route/middleware.go
+++ route/middleware.go
@@ -8,6 +8,7 @@ func AppMiddleware(route *RouterItem) echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			c.Set("route", route)
+			c.Response().Header().Set(echo.HeaderServer, "DuxGo")
 			return next(c)
 		}
 	}
