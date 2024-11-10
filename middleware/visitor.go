ca66ef6052de6fa4a6db7dde766ab640253b6fae
mr.l <admin@duxphp.com>
2024-11-10 11:11:33 +0800

    update

--- middleware/visitor.go
+++ middleware/visitor.go
@@ -3,8 +3,6 @@ package middleware
 import (
 	"github.com/duxweb/go-fast/helper"
 	"github.com/labstack/echo/v4"
-	"net/http"
-	"strings"
 )
 
 func VisitorMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
@@ -13,20 +11,6 @@ func VisitorMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 
 		if method != "GET" {
 			return next(c)
-		}
-
-		path := c.Path()
-		hasInstallLock := helper.IsExist("./data/install.lock")
-		pathContainsInstall := strings.Contains(path, "/install")
-
-		if !hasInstallLock && !pathContainsInstall {
-			err := c.Redirect(http.StatusFound, "/install")
-			return err
-		}
-
-		if hasInstallLock && pathContainsInstall {
-			err := c.Redirect(http.StatusFound, "/")
-			return err
 		}
 
 		err := helper.VisitIncrement(c, "common", 0, "web", "")
